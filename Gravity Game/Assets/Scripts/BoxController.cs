dcfb884bea0998ddc54fa9488d178c0bb8a030e4
achampagne1 <53927114+achampagne1@users.noreply.github.com>
2024-06-13 12:10:25 -0500

    First commit
    First commit

---
+++ Gravity Game/Assets/Scripts/BoxController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxController : MonoBehaviour
+{
+    //object creation
+    Rigidbody2D rb;
+    CircleCollider2D circleColliderPlayer;
+    Transform planetCenter;
+
+    //vectors
+    Vector2 gravityDirection = new Vector2(0, 0);
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject temp = GameObject.Find("Planet");
+        planetCenter = temp.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gravityDirection = (planetCenter.position - transform.position).normalized;
+
+        float angle = Mathf.Atan2(gravityDirection.y, gravityDirection.x) * Mathf.Rad2Deg;
+        Quaternion desiredRotation = Quaternion.Euler(0f, 0f, 90f + angle);
+        transform.rotation = desiredRotation;
+    }
+}
---
+++ Gravity Game/Assets/Scripts/SpaceZombieController.cs
@@ -0,0 +1,161 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceZombieController : MonoBehaviour
+{
+    //object creation
+    Rigidbody2D rb;
+    CircleCollider2D circleColliderPlayer;
+    Transform planetCenter;
+    RandomTimer pauseDuration = new RandomTimer();
+    RandomTimer moveDuration = new RandomTimer();
+
+    //constants
+    public float moveSpeed = 10f;
+    public float jumpForce = 11f;
+    public float gravityForceMag = 20f;
+
+    //game variables
+    float heightTestPlayer = 0;
+    float horizontalInput = 0;
+    float rotatedX = 0;
+    float rotatedY = 0;
+    int layerMaskPlanet = 0;
+    bool isGrounded = false;
+    bool space = false;
+    int moveInput = 0;
+    bool jumpInput = false;
+    bool pause = true;
+    bool facingLeft = false;
+
+    //vectors
+    Vector2 gravityDirection = new Vector2(0, 0);
+    Vector2 gravityForce = new Vector2(0, 0);
+    Vector2 moveDirection = new Vector2(0, 0);
+    Vector2 jump = new Vector2(0, 0);
+    Vector2 previousV = new Vector2(0, 0);
+    Vector2 drag = new Vector2(0, 0);
+    Vector2 previousMove = new Vector2(0, 0);
+    Vector2 jumpExtraction = new Vector2(0, 0);
+
+
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        circleColliderPlayer = GetComponent<CircleCollider2D>();
+        heightTestPlayer = circleColliderPlayer.bounds.extents.y + 0.05f;
+        layerMaskPlanet = LayerMask.GetMask("Default");
+
+        GameObject temp = GameObject.Find("Planet");
+        planetCenter = temp.GetComponent<Transform>();
+
+        rb.velocity = new Vector2(0, 0); //this can be moifie to have a starting velocity
+
+        pauseDuration.create(.1f, 1f);
+        moveDuration.create(1f, 4f);
+    }
+
+    void FixedUpdate()
+    {
+        //inputs
+        horizontalInput = moveInput;
+        //space = Input.GetKeyDown(KeyCode.Space);
+
+        if (horizontalInput == -1 && facingLeft == false)
+        {
+            transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+            facingLeft = true;
+        }
+        else if (horizontalInput == 1 && facingLeft == true)
+        {
+            transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+            facingLeft = false;
+        }
+
+        //Check if the player is grounded
+        isGrounded = IsGrounded();
+
+        if (!isGrounded)
+            jumpExtraction = rb.velocity - gravityForce - moveDirection;
+        else
+            jumpExtraction = Vector2.zero;
+
+        float jumpMagnitude = CalculateMagnitude(jumpExtraction);
+
+        //Calculate gravitational force towards the planet
+        gravityDirection = (planetCenter.position - transform.position).normalized;
+        gravityForce = gravityDirection * gravityForceMag;
+
+        //movement
+        rotatedX = -gravityDirection.y;
+        rotatedY = gravityDirection.x;
+        moveDirection = new Vector2((horizontalInput * moveSpeed * rotatedX), (horizontalInput * moveSpeed * rotatedY));
+
+
+        // Create a quaternion representing the desired rotation angle around the y-axis
+        float angle = Mathf.Atan2(gravityDirection.y, gravityDirection.x) * Mathf.Rad2Deg;
+        Quaternion desiredRotation = Quaternion.Euler(0f, 0f, 90f + angle);
+        transform.rotation = desiredRotation;
+
+        // Jump if grounded and space is pressed
+        rotatedX = -gravityDirection.x;
+        rotatedY = -gravityDirection.y;
+        if (space && isGrounded)
+            jump = new Vector2(rotatedX * jumpForce, rotatedY * jumpForce);
+        else
+            jump = new Vector2(0, 0);
+
+
+        //drag calculation
+        if (isGrounded)
+        {
+            drag = previousV;
+        }
+        else
+            drag = previousMove;
+
+
+        rb.AddForce(gravityForce);
+        rb.AddForce(jump + moveDirection + drag, ForceMode2D.Impulse);
+
+        rb.velocity += -jumpExtraction + jumpMagnitude * -gravityDirection;
+        previousV = -rb.velocity;
+        previousMove = -moveDirection;
+        randomMovement();
+    }
+
+    bool IsGrounded()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(circleColliderPlayer.bounds.center, gravityDirection, heightTestPlayer, layerMaskPlanet);
+        bool ground = hit.collider != null;
+        return ground;
+    }
+
+    float CalculateMagnitude(Vector2 vector)
+    {
+        return Mathf.Sqrt(vector.x * vector.x + vector.y * vector.y);
+    }
+
+    void randomMovement()
+    {
+        if (moveDuration.checkTimer()&&pause) //move state
+        {
+            pause = false;
+            Debug.Log(pause);
+            moveInput = UnityEngine.Random.Range(-1, 2);
+            moveDuration.resetTimer();
+            pauseDuration.resetTimer();
+        }
+        else if (pauseDuration.checkTimer()&&!pause) //!move state
+        {
+            pause = true;
+            Debug.Log(pause);
+            moveInput = 0;
+            moveDuration.resetTimer();
+            pauseDuration.resetTimer();
+        }
+    }
+}   
