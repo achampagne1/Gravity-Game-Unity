8b76a8af0705f012df550f013bfcdeae63cc0e7d
achampagne1 <53927114+achampagne1@users.noreply.github.com>
2024-07-01 12:52:01 -0500

    Health bar fades in and out now

--- Gravity Game/Assets/Scripts/UIHandler.cs
+++ Gravity Game/Assets/Scripts/UIHandler.cs
@@ -5,9 +5,11 @@ using UnityEngine.UIElements;
 
 public class UIHandler : MonoBehaviour
 {
-    public float CurrentHealth =1f;
+    public float currentHealth =1f;
     public static UIHandler instance { get; private set; }
+    float fadeCounter = 180f;
     VisualElement fullBar;
+    VisualElement warningBar;
 
     private void Awake()
     {
@@ -19,18 +21,38 @@ public class UIHandler : MonoBehaviour
     {
         UIDocument uiDocument = GetComponent<UIDocument>();
         fullBar = uiDocument.rootVisualElement.Q<VisualElement>("healthBar");
-        
+        warningBar = uiDocument.rootVisualElement.Q<VisualElement>("warningBar");
+        warningBar.style.opacity = 0f;
+
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (currentHealth <= .3f)
+        {
+            warningBarFunction();
+        }
     }
 
     public void setHealthValue(float health)
     {
-        CurrentHealth = health / 10f;
-        fullBar.style.width = Length.Percent(CurrentHealth * 100.0f);
+        currentHealth = health / 10f;
+        fullBar.style.width = Length.Percent(currentHealth * 100.0f);
+    }
+
+    void warningBarFunction()
+    {
+        if(currentHealth == .3f)
+            fadeCounter -= 2f;
+        else if (currentHealth == .2f)
+            fadeCounter -= 4f;
+        else if (currentHealth == .1f)
+            fadeCounter -= 8f;
+
+        warningBar.style.opacity = 1f - Mathf.Sin(fadeCounter * Mathf.Deg2Rad); 
+        warningBar.style.width = Length.Percent(currentHealth * 100.0f);
+        if (fadeCounter <= 0)
+            fadeCounter = 180;
     }
 }
