085121e96f283f400e6f47ef18b775208f240d2d
achampagne1 <53927114+achampagne1@users.noreply.github.com>
2024-10-08 15:19:30 -0500

    reimplementing bullets

---
+++ Gravity Game/Assets/Scripts/SpaceManController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SpaceManController : CharacterController
+{
+    bool enemyCollideFlag = false;
+    float health = 10f;
+
+    Rigidbody2D Bullet;
+
+    void Start()
+    {
+        GameObject BulletObject = GameObject.Find("Bullet");
+        Bullet = BulletObject.GetComponent<Rigidbody2D>();
+        for (var i = 0; i < 10; i++)
+        {
+            GameObject ShotBullet = Instantiate(BulletObject, new Vector3(i * 2.0f, 0, 0), Quaternion.identity);
+            ShotBullet.GetComponent<BulletController>().setFirst();
+        }
+        calculateStart();
+        
+    }
+
+    void FixedUpdate()
+    {
+        setMovement(inputSystemToGetAxis());
+        Debug.Log(Keyboard.current.aKey.isPressed);
+        setJump(Keyboard.current.spaceKey.isPressed);
+        calculateUpdate();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "SpaceZombie" && enemyCollideFlag == false)
+        {
+            //Vector2 forceVector = new Vector2(-5f,0f);
+            //addForceLocal(forceVector);
+            health -= 1f;
+            UIHandler.instance.setHealthValue(health);
+            enemyCollideFlag = true;
+        }
+        else if (collision.gameObject.name != "SpaceZombie" && enemyCollideFlag == true)
+            enemyCollideFlag = false;
+
+        if (collision.gameObject.name == "MedPack")
+        {
+            health = 10f;
+            UIHandler.instance.setHealthValue(health);
+        }
+            
+
+    }
+
+    int inputSystemToGetAxis()
+    {
+        if (Keyboard.current.aKey.isPressed)
+            return -1;
+        if (Keyboard.current.dKey.isPressed)
+            return 1;
+        else
+            return 0;
+    }
+
+}
---
+++ Gravity Game/Assets/Scripts/SpaceZombieController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceZombieController : CharacterController
+{
+    //object creation
+    RandomTimer pauseDuration = new RandomTimer();
+    RandomTimer moveDuration = new RandomTimer(); 
+
+    int moveInput = 0;
+    bool pause = false;
+    bool following = false;
+    public bool movementToggle = true;
+
+    void Start()
+    {
+        calculateStart();
+        pauseDuration.create(.1f, 1f);
+        moveDuration.create(1f, 4f);
+    }
+
+    void FixedUpdate()
+    {
+        if (movementToggle) //for debugging purposes
+        {
+            if (detectPlayer() && following == false)
+            {
+                if (getFacingLeft())
+                    moveInput = -1;
+                else
+                    moveInput = 1;
+                following = true;
+            }
+            else if (!detectPlayer() && following == true)
+            {
+                moveInput = 0;
+                following = false;
+            }
+            else
+                randomMovement();
+            setMovement(moveInput);
+        }
+        calculateUpdate();
+    }
+
+    void randomMovement()
+    {
+        if (moveDuration.checkTimer()&&pause) //move state
+        {
+            pause = false;
+            moveInput = UnityEngine.Random.Range(-1, 2);
+            moveDuration.resetTimer();
+            pauseDuration.resetTimer();
+        }
+        else if (pauseDuration.checkTimer()&&!pause) //!move state
+        {
+            pause = true;
+            moveInput = 0;
+            moveDuration.resetTimer();
+            pauseDuration.resetTimer();
+        }
+    }
+
+    bool detectPlayer()
+    {
+        for (int i = 0; i<60; i++)
+        {
+            float angle =  (getCharacterOrientation()+30 - i +(System.Convert.ToSingle(getFacingLeft()) *180)) % 360;
+            Vector2 temp = new Vector2(Mathf.Cos(angle * Mathf.PI / 180), Mathf.Sin(angle * Mathf.PI / 180));
+            RaycastHit2D lookForPlayer = Physics2D.Raycast(getCharacterCollider().bounds.center,temp, 100f, LayerMask.GetMask("player"));
+            RaycastHit2D lookForObstacles = Physics2D.Raycast(getCharacterCollider().bounds.center, temp, 100f, LayerMask.GetMask("Default"));
+            if (lookForPlayer.collider != null&& lookForObstacles.collider == null)
+                return true;
+        }
+        return false;
+    }
+}   
